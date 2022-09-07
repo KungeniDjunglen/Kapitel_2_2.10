3205afa68c3c2eb2519dcf16d44aaad555690c8c
EmilWallett <nhlpool27@gmail.com>
2022-09-07 12:08:11 +0200

    Add project files.

---
+++ Uppgift1-2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Uppgift1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Theodor");
+            Console.WriteLine("Denna rad har jag skrivit");
+        }
+    }
+}
